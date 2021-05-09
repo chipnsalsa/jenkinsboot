077d513194c8c8ee0f96aff86b368bf3ee7e4f4b
chipnsalsa <chipnsalsablog@gmail.com>
2021-05-09 11:28:49 +0530

    first commit

---
+++ src/main/java/com/jenkinsboot/jenkinsboot/JenkinsbootApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsboot.jenkinsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkinsboot/jenkinsboot/JenkinsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinsboot.jenkinsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
